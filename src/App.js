27cdfe5847d145edd7c6f8585452765e14ff8d2e
anshuman-sriv <anshuman.8@live.com>
2019-04-18 02:10:08 +0530

    config

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './Places.css'
 import Map from './Map';
 import ReactDependentScript from 'react-dependent-script';
 
+const keys = require('./config/keys');
+console.log(keys.placesUrl);
+
 const App = ()=> {
   return(
     <ReactDependentScript
       scripts={[
-          "https://maps.googleapis.com/maps/api/js?key=AIzaSyD1DrDBUd6GNL2EIBCxK-K0OjkTny8kbuA&callback=initMap"
+          keys.placesUrl
         ]}
     >
       <Map/> 
---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+//client id: 
+module.exports ={
+	mapKey: 'AIzaSyAGT0miR5eQIqYeRk8PrUThza0OHUSI-ms',
+	placesUrl: "https://maps.googleapis.com/maps/api/js?key=AIzaSyD1DrDBUd6GNL2EIBCxK-K0OjkTny8kbuA&callback=initMap"
+};
